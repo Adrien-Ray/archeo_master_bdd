16e176939012f299a13357acfe4f8f2ce05e0f0b
Adrien-Ray <a.raymond@codeur.online>
2022-12-28 13:06:25 +0100

    automatic setup

--- assets/scripts/nav.js
+++ assets/scripts/nav.js
@@ -3,10 +3,25 @@ nav.id = 'nav';
 
 document.body.prepend(nav);
 
-const queryString = window.location.search;
-console.log('queryString', queryString);
+// recup path:
+const path = window.location.pathname;
+console.log('queryString', path);
+
+// determine class:
+let classToIndex = '';
+let classToPortail = '';
+if (path.search('portail.html') != -1) {
+    console.log(path.search('portail.html'));
+    console.log('determine class:portail.html');
+    classToPortail = 'nav__thisPage';
+} else if (path.search('site.html') != -1) {
+    console.log('determine class:site.html');
+} else {
+    console.log('determine class:index.html');
+    classToIndex = 'nav__thisPage';
+}
 
 document.getElementById('nav').innerHTML = `
-    <a href="./">new data</a>
-    <a href="./portail.html">overview</a>
+    <a class="${classToIndex}" href="./">setup data</a>
+    <a class="${classToPortail}" href="./portail.html">overview</a>
 `;
