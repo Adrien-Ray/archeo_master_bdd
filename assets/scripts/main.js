0d7dbc90323a2a6ed12ea711d733f30aa72397c4
Adrien-Ray <a.raymond@codeur.online>
2022-12-28 13:35:57 +0100

    automate save data in localStorage when no data

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,19 +1,7 @@
-import { csvToJson } from "./csvToJson.js";
-import { saveInLocalStorage } from "./saveInLocalStorage.js";
 import { automaticSetup } from "./automaticSetup.js";
+import { csvToStorage } from "./csvToStorage.js";
 
-function csvToStorage(lieuxCsv, materielsCsv) {
-    // console.log(lieuxCsv, materielsCsv);
-    const jsonLieux = csvToJson(lieuxCsv);
-    const jsonMateriels = csvToJson(materielsCsv);
-    const tockenToLocalStorage = saveInLocalStorage(jsonLieux, jsonMateriels);
-    if (tockenToLocalStorage === true) {
-        alert('vos données sont enregistrer temporairement');
-        location.href = './portail.html';
-    } else {
-        alert('un problème est survenu');
-    }
-}
+
 
 
 
